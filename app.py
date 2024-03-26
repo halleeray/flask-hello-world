d0990a0e682609229c07ff7154bec0435111960e
Hallee Ray <hara7620@colorado.edu>
2024-03-26 16:36:31 +0000

    lab 10 1st commit

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+import psycopg2
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgres://lab10_sf5i_user:1SS5isBvW8EITDxpvIgXJHNmmgX4LxMK@dpg-co1fh7021fec73d252kg-a/lab10_sf5i") 
+    conn.close()
+    return "Database Connection Successful"
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World from Hallee Ray in 3308!'
